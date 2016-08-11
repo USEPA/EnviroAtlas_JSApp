25c71403fbec062845b5697a7a5a82de9f3dc8a7
David Yarnell <dyarnell@epa.gov>
2016-08-11 10:01:55 -0700

    made drawer widget more dynamic, re-arranged on screen widgets, code clean up

--- widgets/Drawer/Widget.js
+++ widgets/Drawer/Widget.js
@@ -1,6 +1,6 @@
 define(['dojo/_base/declare', 'jimu/BaseWidget', 'jimu/WidgetManager',
-      'jimu/PanelManager', 'dojo/on', 'dojo/dom'],
-function(declare, BaseWidget, WidgetManager, PanelManager, on, dom) {
+      'jimu/PanelManager', 'dojo/on', 'dojo/dom', 'dojo/dom-construct'],
+function(declare, BaseWidget, WidgetManager, PanelManager, on, dom, domConstruct) {
   //To create a widget, you need to derive from BaseWidget.
   return declare([BaseWidget], {
     // DemoWidget code goes here
@@ -19,44 +19,35 @@ function(declare, BaseWidget, WidgetManager, PanelManager, on, dom) {
       var self = this;
       this.inherited(arguments);
 
-      //on click events
-      var AddDataButton = dom.byId('widget_AddData');
-      on(dom.byId(AddDataButton), "click", function(){
-        var widgets = self.appConfig.getConfigElementsByName('AddData');
-        var pm = PanelManager.getInstance();
-        
-        if(widgets[0].visible){
-          pm.closePanel(widgets[0].id + "_panel");
-        }
-        pm.showPanel(widgets[0]);
-        pm.closePanel(self.id + "_panel");
-      });
+      //test creating from config
+      var widgetsArray = this.config.includeWidgets;
+      var topLocation = 0;
 
-      var AddFileButton = dom.byId('widget_AddFile');
-      on(dom.byId(AddFileButton), "click", function(){
-        var widgets = self.appConfig.getConfigElementsByName('AddShapefile');
-        var pm = PanelManager.getInstance();
-        //console.log(widgets[0]);
-        if(widgets[0].visible){
-          pm.closePanel(widgets[0].id + "_panel");
-        }
-        pm.showPanel(widgets[0]);
-        pm.closePanel(self.id + "_panel");
+      //loop through widgets to add to drawer
+      widgetsArray.forEach(function(w){
+        //Add buttons into widget
+        domConstruct.place('<div style="left: 0px; top: ' + topLocation + 'px; right: auto; bottom: auto; width: 100%; height: 40px; padding: 0px; z-index: auto;line-height: 40px" class="jimu-widget-onscreen-icon" id="widget_' + w + '"><img style="float:left" src="widgets/' + w + '/images/icon.png"><span class="droplabel" >' + w + '</span></div>', dom.byId("drawerNode"));
+        //create click event
+        self._setWidgetOpenHandler(w);
+        //move next button down 40px
+        topLocation = topLocation + 40;
       });
-
-      var AddServiceButton = dom.byId('widget_AddService');
-      on(dom.byId(AddServiceButton), "click", function(){
-        var widgets = self.appConfig.getConfigElementsByName('AddService');
-        var pm = PanelManager.getInstance();
-        //console.log(widgets[0]);
-        if(widgets[0].visible){
-          pm.closePanel(widgets[0].id + "_panel");
-        }
-        pm.showPanel(widgets[0]);
-        pm.closePanel(self.id + "_panel");
-      });
-
       console.log('startup');
+    },
+
+    _setWidgetOpenHandler: function(widgetName){
+      var self = this;
+      var AddFileButton = dom.byId('widget_' + widgetName);
+      on(dom.byId(AddFileButton), "click", function(){
+        var widgets = self.appConfig.getConfigElementsByName(widgetName);
+        var pm = PanelManager.getInstance();
+        //console.log(widgets[0]);
+        if(widgets[0].visible){
+          pm.closePanel(widgets[0].id + "_panel");
+        }
+        pm.showPanel(widgets[0]);
+        pm.closePanel(self.id + "_panel");
+      });
     },
 
     onOpen: function(){
