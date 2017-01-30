855f03a25ae00bc0b4a1c8e304411c9637462047
David Yarnell <dyarnell@epa.gov>
2017-01-29 19:26:25 -0800

    Added new Drawer Widget Mapping Tools Re-organized widgets in the three drawer widgets

---
+++ widgets/DrawerMappingTools/nls/da/strings.js
@@ -0,0 +1,7 @@
+﻿define(
+  ({
+    _widgetLabel: "Demo",
+    label1: "Jeg er en demo-widget.",
+    label2: "Dette kan konfigureres."
+  })
+);
---
+++ widgets/DrawerMappingTools/nls/ko/strings.js
@@ -0,0 +1,7 @@
+﻿define(
+  ({
+    _widgetLabel: "Demo",
+    label1: "데모 위젯입니다.",
+    label2: "구성 가능합니다."
+  })
+);
