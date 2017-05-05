e56fe3b3bae250d35b21440751bbc2e9dd76eb0e
Baohong <bji@innovateteam.com>
2017-05-05 16:20:09 -0400

    open User Interface window for Time Series Viewer; fix ECAT bug for information icon button for IE

--- widgets/Drawer/Widget.js
+++ widgets/Drawer/Widget.js
@@ -44,14 +44,14 @@ function(declare, BaseWidget, WidgetManager, PanelManager, on, dom, domConstruct
       var AddFileButton = dom.byId('widget_' + widgetName);
       
       on(dom.byId(AddFileButton), "click", function(){
-      	if (widgetName == "TimeSeriesViewer"){
-      		window.open("https://leb.epa.gov/Projects/EnviroAtlas/CurrentDevelopment/FSViewer/index.html");
-      	} else {
+      	//if (widgetName == "TimeSeriesViewer"){
+      	//	window.open("https://leb.epa.gov/Projects/EnviroAtlas/CurrentDevelopment/FSViewer/index.html");
+      	//} else {
 	        var widgets = self.appConfig.getConfigElementsByName(widgetName);
 	        var pm = PanelManager.getInstance();
 	        //open selected widget
 	        pm.showPanel(widgets[0]);
-       }
+       //}
       });
     },
 
