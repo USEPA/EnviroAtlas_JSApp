8ace3ebf41b3da4b8181da0d60b4df642a52f58b
Baohong <bji@innovateteam.com>
2017-05-17 10:46:50 -0400

    Popup info window set for whole U.S.A extent

--- widgets/SimpleSearchFilter/Widget.js
+++ widgets/SimpleSearchFilter/Widget.js
@@ -27,6 +27,8 @@ define([
 	 'esri/symbols/SimpleLineSymbol',
 	 'esri/symbols/SimpleFillSymbol',
 	 'esri/renderers/SimpleRenderer',
+	 'esri/tasks/QueryTask',
+     'esri/tasks/query',
 	 'esri/graphic',
 	 'esri/Color',
 	 'esri/renderers/ClassBreaksRenderer',     
@@ -55,6 +57,8 @@ define([
     SimpleLineSymbol,
     SimpleFillSymbol,
     SimpleRenderer,
+    QueryTask,
+    query,
     graphic,
     Color,
     ClassBreaksRenderer) {
@@ -68,6 +72,95 @@ define([
             label: "name", //Name field for display. Not pertinent to a grid but may be used elsewhere.
             items: []};
     	var layerDataStore = new dojo.data.ItemFileWriteStore({ data:layerData });
+    	var featuresCollection = [];
+    	var arrLayersForPopup = [];
+    	var addSingleFeatureForPopup = function(eaID, clickEvt) {
+    		
+			var selectQuery = new query();
+            selectQuery.geometry = clickEvt.mapPoint;
+            selectQuery.returnGeometry = true;
+            selectQuery.spatialRelationship = query.SPATIAL_REL_INTERSECTS;            
+            
+            var queryTask = new QueryTask(window.hashURL[eaID]);
+            popupField = window.hashPopup[eaID].fieldInfos[0]["fieldName"];
+            popupFieldName = window.hashPopup[eaID].fieldInfos[0]["label"];
+            popupTitle = window.hashPopup[eaID].title.split(":");
+            selectQuery.outFields = ["*"];
+            selectQuery.outFields = [popupField, popupTitle[1].trim().replace("{","").replace("}","")];
+            
+            queryTask.execute(selectQuery, function (features) {
+            	if (window.hashPopup[eaID] != undefined) {
+										
+					//Performance enhancer - assign featureSet array to a single variable.
+					var resultFeatures = features.features;
+					var symbol = new SimpleFillSymbol(
+	                  SimpleFillSymbol.STYLE_NULL, 
+	                  new SimpleLineSymbol(
+	                    SimpleLineSymbol.STYLE_SOLID, 
+	                    new Color([0, 0, 200, 255]), 
+	                    1
+	                  ),
+	                  new Color([215, 215, 215,255])
+	                );
+				
+					//Loop through each feature returned
+					for (var i=0, il=resultFeatures.length; i<il; i++) {
+						var content = "<b>" + popupTitle[0] + "</b>: $" + popupTitle[1].trim() + "<hr>"+"<b>" + popupFieldName + "</b>: ${" + popupField + "}";			
+						var infoTemplate = new esri.InfoTemplate(popupFieldName, content);
+					
+					    var graphic = resultFeatures[i];
+					    graphic.setSymbol(symbol);
+					    graphic.setInfoTemplate(infoTemplate);
+					    featuresCollection.push(graphic);
+					
+					    self.map.graphics.add(graphic);
+					}
+
+					if 	(arrLayersForPopup.length > 0){
+	        			addSingleFeatureForPopup(arrLayersForPopup.pop(),clickEvt);
+	        		}
+	        		else {
+	        			if 	(featuresCollection.length > 0){
+			    			self.map.infoWindow.setFeatures(featuresCollection);
+							self.map.infoWindow.show(clickEvt.mapPoint);
+						}
+					}
+                }
+            }); 	
+    	};
+    	
+    	var setClickEvent = function(){    		
+
+    		intersect = self.map.on("click", function(evt) {
+    			self.map.graphics.clear();
+    			featuresCollection = [];
+    			arrLayersForPopup = [];
+	    		for (i in window.allLayerNumber) {  
+	    			bVisibleFL = false;
+	    			bVisibleTL = false;
+	    			  		
+		    		lyrFL = self.map.getLayer(window.layerIdPrefix + window.allLayerNumber[i]);		    		
+		    		if (lyrFL != null) {		    			
+						if (lyrFL.visible == true){
+							bVisibleFL = true;
+						}
+					}
+
+					lyrTL = self.map.getLayer(window.layerIdTiledPrefix + window.allLayerNumber[i]);
+		    		if (lyrTL != null) {		    			
+						if (lyrTL.visible == true){
+							bVisibleTL = true;							
+						}
+					}		
+					
+					if ((bVisibleFL == true) || (lyrTL == true)) {
+						arrLayersForPopup.push(window.allLayerNumber[i]);
+					}		    		
+		    	}
+		    	//start to popup for first layer:
+		    	addSingleFeatureForPopup(arrLayersForPopup.pop(),evt);         
+    		})
+    	};
     	var SelectableLayerFactory = function(data) {
 		    this.eaLyrNum = data.eaLyrNum;
 		    this.name = data.name;
@@ -1030,6 +1123,7 @@ define([
 			searchBox.style.padding = '2px 2px 2px 2px';
 
 			_updateSelectableLayer();
+			setClickEvent();
 
         });// end of loadJSON(function(response)
         loadCommunityJSON(function(response){
