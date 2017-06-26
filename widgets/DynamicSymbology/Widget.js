08fd7372aa3530490c1d7c68dd35107884fc6a12
David-Yarnell <dyarnell@innovateteam.com>
2017-06-26 10:02:13 -0700

    fix issue with reset button not applying the right original symbology when community selected but changing symbology on national layer

--- widgets/DynamicSymbology/Widget.js
+++ widgets/DynamicSymbology/Widget.js
@@ -290,27 +290,22 @@ function(declare, BaseWidget, LayerInfos, dom, domConstruct, on, domStyle, Map, 
 				       		lyrTiled.setVisibility(true);
 			      	  }       	
 			  }
-			  if (window.communitySelected != window.strAllCommunity) {
-				  $.getJSON( 'configs/CommunitySymbology/' + window.communitySelected + '_JSON_Symbol/Nulls/' + window.communitySelected + '_' + window.hashAttribute[res[1]] + ".json", function( data ) {
+
+			  var str = lyrTobeUpdated.url;
+			  var lookfor = "National";
+			  if(str.indexOf(lookfor)>-1){
+				 console.log("get from json");
+				  var defaultRenderer = new ClassBreaksRenderer(currentSymbology[_layerID]['origRenderer']);
+				  selfDynamicSymbology._resetElements(defaultRenderer);
+			  }else{
+				  //get from community
+				  $.getJSON( 'configs/CommunitySymbology/' + 'AllCommunities' + '_JSON_Symbol/Nulls/' + 'CombComm' + '_' + window.hashAttribute[res[1]] + ".json", function( data ) {
 					  var defaultRenderer = new ClassBreaksRenderer(data);
 					  selfDynamicSymbology._resetElements(defaultRenderer);
 				  })
-			  }else {
-			      var str = lyrTobeUpdated.url;
-                  var lookfor = "National";
-			      if(str.indexOf(lookfor)>-1){
-			         console.log("get from json");
-                      var defaultRenderer = new ClassBreaksRenderer(currentSymbology[_layerID]['origRenderer']);
-                      selfDynamicSymbology._resetElements(defaultRenderer);
-                  }else{
-                      //get from community
-                      $.getJSON( 'configs/CommunitySymbology/' + 'AllCommunities' + '_JSON_Symbol/Nulls/' + 'CombComm' + '_' + window.hashAttribute[res[1]] + ".json", function( data ) {
-                          var defaultRenderer = new ClassBreaksRenderer(data);
-                          selfDynamicSymbology._resetElements(defaultRenderer);
-                      })
-                  }
-
 			  }
+
+
 		  });
 	  });
 
