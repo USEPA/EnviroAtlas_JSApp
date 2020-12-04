2c752fed8bbfa137c6e3ce44520ebebe1516616e
Baohong <ji.baohong@epa.gov>
2020-12-04 17:42:32 -0500

    debug Help link anchor issue

--- jimu.js/utils.js
+++ jimu.js/utils.js
@@ -170,18 +170,6 @@ function(lang, array, html, has, config, ioQuery, query, nlt, Deferred, all, on,
     }
   }
 
-  function displayMoreInformation() {
-		    var parthArray = window.location.pathname.split("/");	
-		    var parth1= "";
-		    if (parthArray[0] == "") {
-		    	parth1 = parthArray[1];		    	
-		    }
-		    else {
-		    	parth1 = parthArray[0];	
-		    }
-        	window.open(window.location.protocol + '//' + window.location.host + "/" + parth1 + "/help.html"+ "#"+ window.widgetNameInDemo);
-    		return false;
-  }
   //if no beforeId, append to head tag, or insert before the id
   function loadStyleLink(id, href, beforeId) {
     var def = new Deferred(), styleNode, styleLinkNode;
@@ -5301,6 +5289,18 @@ function(lang, array, html, has, config, ioQuery, query, nlt, Deferred, all, on,
 			  newlink.setAttribute('class', 'topicHeader');
 			  newlink.setAttribute('style', 'width:100%;background-color: #9aadbb; margin-top:20px');
 			  newlink.setAttribute('id', 'helpContent2TriggerButton');
+			  //newlink.setAttribute('onclick', "var parthArray = window.location.pathname.split('/'); var parth1= ''; if (parthArray[0] == '') {	parth1 = parthArray[1];} else {parth1 = parthArray[0];  }     	window.open(window.location.protocol + '//' + window.location.host + '/' + parth1 + '/help.html'+ '#'+ window.widgetNameInDemo);return false;");
+			  var clickAction = "var parthArray = window.location.pathname.split('/'); " +
+			  "var parth1= ''; if (parthArray[0] == '') " +
+			  "{	parth1 = parthArray[1];}" + 
+			  "else {parth1 = parthArray[0];}" +     	
+			  "window.open(window.location.protocol + '//' + window.location.host + '/' + parth1 + '/help.html'+ '#'+ window.widgetNameInDemo);" +
+			  "return false;";
+			  
+			  newlink.setAttribute('onclick', clickAction);
+    		
+    		
+			  
 			  newDiv.appendChild(newlink);
 			  helpContent.domNode.appendChild(newDiv);
                 
@@ -5361,7 +5361,6 @@ function(lang, array, html, has, config, ioQuery, query, nlt, Deferred, all, on,
 		                window.displayMoreInfor = "true";
 		            }      
 	            }  
-	            document.getElementById("helpContent2TriggerButton").addEventListener("click", displayMoreInformation); 
                                                     
               exitButtons = document.getElementsByClassName("exit_buttonOnScreenWidget");
 
