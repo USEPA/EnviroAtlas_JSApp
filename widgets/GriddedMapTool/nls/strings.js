729573fbaa6a0e62ad89fa30da4e8e5d5bc26051
Baynes <baynes.jeremy@epa.gov>
2023-01-10 14:59:01 -0500

    Updated Links, 118 congress, year2 is visible

--- widgets/GriddedMapTool/nls/strings.js
+++ widgets/GriddedMapTool/nls/strings.js
@@ -59,7 +59,6 @@ define({
         90: "Woody Wetlands",
         95: "Emergent Herbaceous Wetlands",
       },
-      /*layer: 'https://ags108.blueraster.io/server/rest/services/EnviroAtlas/TO7_NLCD_Flood/ImageServer', */
       layer: 'https://leb.epa.gov/arcgis/rest/services/Supplemental/GDT_NLCD_Dasy/ImageServer',
       OBJECTIDS: {
         2001: 1,
@@ -69,7 +68,7 @@ define({
         2011: 5,
         2013: 6,
         2016: 7,
-		2019: 8,
+		    2019: 8,
       },
       changeIndex: 9,
       floodplains: 10,
@@ -98,22 +97,18 @@ define({
       columnHeaders: ['Land Cover Type', 'Area (km2)', 'Percentage']
     },
     "population-floodplains": {
-      /* layer: 'https://ags108.blueraster.io/server/rest/services/EnviroAtlas/TO7_Pop_Flood_Roads_Padus/ImageServer', */
 	  layer: 'https://leb.epa.gov/arcgis/rest/services/Supplemental/GDT_Pop/ImageServer', 
       lockRasterId: 2,
       layersUsed: ['Estimated Floodplains', 'Dasymetric Population']
     },
     "population-roads": {
-      /* layer: 'https://ags108.blueraster.io/server/rest/services/EnviroAtlas/TO7_Pop_Flood_Roads_Padus/ImageServer', */
 	  layer: 'https://leb.epa.gov/arcgis/rest/services/Supplemental/GDT_Pop/ImageServer', 
       lockRasterId: 4,
       layersUsed: ['FAF4 Roads', 'Dasymetric Population']
     },
     populationRasterId: 10,
     padus: {
-      /* layer: 'https://ags108.blueraster.io/server/rest/services/EnviroAtlas/TO7_Pop_Flood_Roads_Padus/ImageServer', */
 	  layer: 'https://leb.epa.gov/arcgis/rest/services/Supplemental/GDT_Pop/ImageServer', 
-      /* polys: 'https://ags108.blueraster.io/server/rest/services/EnviroAtlas/PADUS_Shapefile/MapServer/0', */
 	  polys: 'https://leb.epa.gov/arcgis/rest/services/Supplemental/PADUS_Shapefile/MapServer/0',
       lockRasterId: 3,
       layersUsed: ['PADUS']
@@ -132,9 +127,9 @@ define({
     },
     stateLayer: 'https://services.arcgis.com/P3ePLMYs2RVChkJx/ArcGIS/rest/services/USA_States_Non_Generalized/FeatureServer/0',
     countyLayer: 'https://services.arcgis.com/P3ePLMYs2RVChkJx/arcgis/rest/services/USA_Counties/FeatureServer/0',
-    districtLayer: 'https://services.arcgis.com/P3ePLMYs2RVChkJx/arcgis/rest/services/USA_117th_Congressional_Districts/FeatureServer/0',
-    districtVersion: '117th Congressional District',
-    "huc-12Layer": 'https://enviroatlas2.epa.gov/arcgis/rest/services/test_services/allResults/MapServer/0',
-    "huc-8Layer": 'https://leb.epa.gov/arcgis/rest/services/Supplemental/HUC8/MapServer/0'
+    districtLayer: 'https://services.arcgis.com/P3ePLMYs2RVChkJx/arcgis/rest/services/USA_118th_Congressional_Districts/FeatureServer/0',
+    districtVersion: '118th Congressional District',
+    "huc-12Layer": 'https://enviroatlas.epa.gov/arcgis/rest/services/Other/HydrologicUnits/MapServer/4',
+    "huc-8Layer": 'https://enviroatlas.epa.gov/arcgis/rest/services/Other/HydrologicUnits/MapServer/2'
   },
 });
