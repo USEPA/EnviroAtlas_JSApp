bea45cf988d6ce76204cd5dc23c495d0ef77376b
brosenbaum <rosenbaum.barbara@epa.gov>
2024-05-06 14:26:19 -0400

    change to Raindrop widget URL and data structure

--- widgets/Raindrop/Widget.js
+++ widgets/Raindrop/Widget.js
@@ -212,23 +212,20 @@ function(declare, BaseWidget, on, lang, utils, PanelManager, esriRequest, dojoJs
 
     _run_RaindropService: function (point){
 
-      //var service_url = 'https://ordspub.epa.gov/ords/waters10/PointIndexing.Service';
-	  //var staging service_url = 'https://ordsstage.epa.gov/ords/waters10/PointIndexing.Service';
+      //var service_url = 'https://api.epa.gov/waters/v1/pointindexing?pgeometry=POINT%28-83.38745055283775%2033.94669558441081%29&api_key=zXhLdLJEzIyZZ8KKjml1nvKg2sGT7RTWVUI3tWet';
+	  //var staging service_url = 'https://api.epa.gov/waters/v1/pointindexing?api_key=zXhLdLJEzIyZZ8KKjml1nvKg2sGT7RTWVUI3tWet&pgeometry=POINT%28-83.38745055283775%2033.94669558441081%29';
       //settings for indexing service
       var data = {
-        "pGeometry": "POINT(" + point.getLongitude() + " " + point.getLatitude() + ")",
-        "pGeometryMod": "WKT,SRSNAME=urn:ogc:def:crs:OGC::CRS84",
-        "pPointIndexingMethod": "RAINDROP",
-        "pPointIndexingRaindropDist": maxD, //Max Distance
-        "pPointIndexingMaxDist": snapD,   //Max Snap Dist
-        "pOutputPathFlag": "TRUE",
-        "pReturnFlowlineGeomFlag": "FALSE",
-        "optOutCS": "SRSNAME=urn:ogc:def:crs:OGC::CRS84",
-        "optOutPrettyPrint": 0,
-        "optClientRef": "CodePen"
+        "pgeometry": "SRID=4326;POINT(" + point.getLongitude() + " " + point.getLatitude() + ")",
+        //"pgeometryMod": "WKT,SRSNAME=urn:ogc:def:crs:OGC::CRS84",
+        "ppointindexingmethod": "RAINDROP",
+        "ppointindexingraindropdist": maxD, //Max Distance
+        "ppointindexingmaxdist": snapD,   //Max Snap Dist
+        "poutputpathflag": "TRUE",
+        "preturnflowlinegeomflag": "FALSE",
       };
       //Point Indexing service
-      var layerUrl = "https://ordsstage.epa.gov/ords/waters10/PointIndexing.Service";
+      var layerUrl = "https://api.epa.gov/waters/v1/pointindexing?api_key=zXhLdLJEzIyZZ8KKjml1nvKg2sGT7RTWVUI3tWet";
       var layersRequest = esriRequest({
         url: layerUrl,
         content: data,
