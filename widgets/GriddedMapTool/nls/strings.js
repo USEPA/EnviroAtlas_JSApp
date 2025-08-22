91fdac403b4f01095b57d2049eb2132ad97133c4
tlomba01 <lombardi.thera@epa.gov>
2025-08-22 15:44:09 -0400

    Extra single quote.

--- widgets/GriddedMapTool/nls/strings.js
+++ widgets/GriddedMapTool/nls/strings.js
@@ -189,7 +189,7 @@ define({
       }
     },
     stateLayer: 'https://services.arcgis.com/P3ePLMYs2RVChkJx/ArcGIS/rest/services/USA_States_Non_Generalized/FeatureServer/0',
-    countyLayer: 'https://services.arcgis.com/P3ePLMYs2RVChkJx/ArcGIS/rest/services/USA_Counties_and_States_with_PR/FeatureServer/0'',
+    countyLayer: 'https://services.arcgis.com/P3ePLMYs2RVChkJx/ArcGIS/rest/services/USA_Counties_and_States_with_PR/FeatureServer/0',
     blockgroupLayer: 'https://ejscreen.epa.gov/arcgis/rest/services/ejscreen/census2020acs/MapServer/0',
     districtLayer: 'https://services.arcgis.com/P3ePLMYs2RVChkJx/arcgis/rest/services/USA_118th_Congressional_Districts/FeatureServer/0',
     districtVersion: '118th Congressional District',
